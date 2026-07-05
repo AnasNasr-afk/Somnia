3d3d18b170445a8f2434d41d769e114600a21f45
Anas Nasr <anas.nasr132003@gmail.com>
2026-07-05 16:46:59 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Anas Nasr on 05/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
